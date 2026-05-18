a1953af31a5f82b57f1e996e26685fe5d19d6b8a
Kaushik Karmakar <kaushikkarmakar1590@outlook.com>
2026-05-19 00:02:37 +0530

    Merged backend and frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import process from "node:process";
 import { configDotenv } from "dotenv";
 import express from "express"
+import type { Request, Response } from "express";
 import cors from "cors"
 import path from "node:path";
 
@@ -10,16 +11,16 @@ import rateLimiter from "./middleware/rateLimiter.ts";
 import errorHandler from "./middleware/errorHandler.ts";
 
 configDotenv({
-    quiet:true,
-    debug:false,
-    encoding:"UTF-8"
+    quiet: true,
+    debug: false,
+    encoding: "UTF-8"
 })
 
 const app = express();
 const port = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-if(process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
     app.use(
         cors({
             origin: "http://localhost:5173"
@@ -31,6 +32,13 @@ app.use(express.json());
 app.use(rateLimiter)
 app.use("/api/notemaker", router);
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.use((req: Request, res: Response) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 connectDB().then(() => {
     app.listen(port, () => {
